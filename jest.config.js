1d50152b03af5fd833c4fbe47bd92767f250cd40
epr3 <eduard.florin.predescu@gmail.com>
2018-12-13 15:52:56 +0200

    added jest config

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  moduleFileExtensions: ['js', 'jsx', 'json', 'vue'],
+  snapshotSerializers: ['jest-serializer-vue'],
+  testMatch: [
+    '**/tests/**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx)'
+  ]
+};
