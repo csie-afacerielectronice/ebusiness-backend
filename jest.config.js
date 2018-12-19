78504bf2495c17bee76ff5bccf5ce5a6feca3c1b
epr3 <eduard.florin.predescu@gmail.com>
2018-12-19 15:29:35 +0200

    finished jest config

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,10 @@ module.exports = {
   moduleFileExtensions: ['js', 'json'],
   testMatch: [
     '**/tests/**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx)'
+  ],
+  collectCoverageFrom: [
+    'src/**/*.js',
+    '!src/(models|config|routes)/**.js',
+    '!src/app.js'
   ]
 };
---
+++ tests/services/product.spec.js
@@ -0,0 +1,8 @@
+const productService = require('../../src/services/product.service');
+
+describe('Product service', () => {
+  test('it should create a new product', done => {
+    expect(2 + 2).toBe(4);
+    done();
+  });
+});
