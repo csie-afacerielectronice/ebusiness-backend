d036976aaf2c1a94f4973b8b49162087b586f25f
epr3 <eduard.florin.predescu@gmail.com>
2018-12-19 17:24:49 +0200

    wip tests

--- tests/services/product.spec.js
+++ tests/services/product.spec.js
@@ -1,8 +1,30 @@
-const productService = require('../../src/services/product.service');
+const db = require('../../src/models');
+const productService = require('../../src/services/product.service.js');
 
 describe('Product service', () => {
-  test('it should create a new product', done => {
-    expect(2 + 2).toBe(4);
+  let categoryObj;
+  beforeAll(async () => {
+    await db.sequelize.sync();
+    categoryObj = await new db.category({
+      name: 'category'
+    }).save();
+  });
+  afterAll(async done => {
+    await db.product.destroy({ truncate: true });
+    await db.category.destroy({ truncate: true });
+    await db.sequelize.close();
+    done();
+  });
+  test('it should create a new product', async done => {
+    const data = {
+      name: 'product',
+      description: 'description',
+      categoryId: categoryObj.id,
+      price: 14.99,
+      image: '/path'
+    };
+    const result = await productService.createProduct(data);
+    expect(result).toBeTruthy();
     done();
   });
 });
