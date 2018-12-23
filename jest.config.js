32558fe0fd5f473560de28860828da44ac681f2d
Eduard Predescu <eduard.florin.predescu@gmail.com>
2018-12-23 16:16:23 +0200

    added models to testing

--- jest.config.js
+++ jest.config.js
@@ -4,7 +4,7 @@ module.exports = {
   testMatch: [
     '**/tests/**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx)'
   ],
-  collectCoverageFrom: ['src/controllers/*.js'],
+  collectCoverageFrom: ['src/(controllers|models)/*.js'],
   coverageDirectory: './tests/coverage',
   globalSetup: './tests/globalSetup.js',
   globalTeardown: './tests/globalTeardown.js'
