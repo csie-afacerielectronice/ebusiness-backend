aed9c41acc53c9aabc2c00170d63569744e7998b
Eduard Predescu <eduard.florin.predescu@gmail.com>
2018-12-23 16:17:21 +0200

    edited configs

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,7 @@
 module.exports = {
   moduleFileExtensions: ['js', 'json'],
   testEnvironment: 'node',
-  testMatch: [
-    '**/tests/**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx)'
-  ],
+  testMatch: ['**/tests/**/*.spec.(js|ts)|**/__tests__/*.(js|ts)'],
   collectCoverageFrom: ['src/(controllers|models)/*.js'],
   coverageDirectory: './tests/coverage',
   globalSetup: './tests/globalSetup.js',
