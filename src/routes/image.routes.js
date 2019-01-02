703d4462f09aff704b9781cc063175dfb720dea7
Eduard Predescu <eduard.florin.predescu@gmail.com>
2019-01-02 18:49:35 +0200

    removed avatar from product

--- src/routes/image.routes.js
+++ src/routes/image.routes.js
@@ -6,13 +6,6 @@ const role = require('../utils/role');
 const imageController = require('../controllers/image.controller');
 
 router.post(
-  '/profile',
-  passport.authenticate('jwt'),
-  roleMiddleware([role.ADMIN, role.CLIENT]),
-  multer.avatar.single('avatar'),
-  imageController.uploadAvatar
-);
-router.post(
   '/products/:id/image',
   passport.authenticate('jwt'),
   roleMiddleware([role.ADMIN]),
