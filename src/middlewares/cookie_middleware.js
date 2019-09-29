c41695616492bd7b2e564d9d829121194cdf7434
Eduard Predescu <eduard.florin.predescu@gmail.com>
2019-09-29 23:29:02 +0300

    created cookie middleware

--- src/middlewares/cookie_middleware.js
+++ src/middlewares/cookie_middleware.js
@@ -1,6 +1,11 @@
+const { UNAUTHORIZED } = require('../utils/errors');
+
 module.exports = async (req, res, next) => {
   try {
-
+    if (!req.cookies.refresh_token) {
+      UNAUTHORIZED();
+    }
+    next();
   } catch (e) {
     next(e);
   }
