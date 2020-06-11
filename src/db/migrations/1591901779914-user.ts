7496673bf7b70a9a80083134366d4ee89df86df3
Eduard Predescu <eduard.florin.predescu@gmail.com>
2020-06-11 22:01:03 +0300

    added user migration

--- src/db/migrations/1591901779914-user.ts
+++ src/db/migrations/1591901779914-user.ts
@@ -1,11 +1,36 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
 
-export class user1591901779914 implements MigrationInterface {
+export class User1591901779914 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: "users",
+        columns: [
+          {
+            name: "id",
+            type: "varchar",
+            isPrimary: true,
+          },
+          {
+            name: "email",
+            type: "varchar",
+            isUnique: true,
+          },
+          {
+            name: "password",
+            type: "varchar",
+          },
+          {
+            name: "role",
+            type: "varchar",
+          },
+        ],
+      }),
+    );
+  }
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-    }
-
-    public async down(queryRunner: QueryRunner): Promise<void> {
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.getTable("users");
+    await queryRunner.dropTable("users");
+  }
 }
