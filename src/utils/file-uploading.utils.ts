64915946e47e9a73455e1d85b5d75364cc31aa49
Eduard Predescu <eduard.florin.predescu@gmail.com>
2020-06-14 21:38:14 +0300

    added types to file upload utils

--- src/utils/file-uploading.utils.ts
+++ src/utils/file-uploading.utils.ts
@@ -1,13 +1,21 @@
 import { extname } from "path";
 
-export const imageFileFilter = (_: any, file: any, callback: any) => {
+export const imageFileFilter = (
+  _: any,
+  file: Express.Multer.File,
+  callback: (error: Error, acceptFile: boolean) => void,
+) => {
   if (!file.originalname.match(/\.(jpg|jpeg|png|gif)$/)) {
     return callback(new Error("Only image files are allowed!"), false);
   }
   callback(null, true);
 };
 
-export const editFileName = (_: any, file: any, callback: any) => {
+export const editFileName = (
+  _: any,
+  file: Express.Multer.File,
+  callback: (error: Error, fileName: string) => void,
+) => {
   const fileExtName = extname(file.originalname);
   const randomName = Array(16)
     .fill(null)
